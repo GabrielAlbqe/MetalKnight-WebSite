8fbe63df274580eac65a0fc5c1dcebda96227a34
Gabriel <gabrielalbuquerque762@gmail.com>
2025-09-28 11:53:48 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './app.css'
 
+
+import Team from './assets/foto-mk.jpg'
 import PageHeader from './layout/PageHeader';
 import MetalButton from './layout/MetalButton'
 
@@ -9,15 +11,16 @@ function App() {
     <>
       <PageHeader></PageHeader>
       <section className="sctInicial">
-        <div className="textInfo">
-          <div className="Hidden">
+        <div className="container">
+          {/* Texto na esquerda */}
+          <div className="textInfo">
             <h1 className="txtInicialPage">
               INOVAMOS HOJE PARA CONSTRUIR UM
             </h1>
             <h1 className="txt1Colorido">
               AMANHÃ MELHOR.
             </h1>
-            <p>
+            <p >
               Nossa missão é criar soluções criativas e eficientes que transformem o presente e impulsionem o
               futuro.
               Com foco na inovação, buscamos desenvolver projetos que inspirem mudanças,
@@ -25,7 +28,12 @@ function App() {
             </p>
             <MetalButton></MetalButton>
           </div>
+
+          {/*Imagem na Direita */}
+          <img className='Team-Photo' src={Team} alt="" />
         </div>
+
+        
       </section>
     </>
   )
--- src/layout/PageHeader.jsx
+++ src/layout/PageHeader.jsx
@@ -1,34 +1,50 @@
+import { useState } from 'react'
 import logo from '../assets/logo.png'
 import './PageHeader.css'
 
 function PageHeader() {
-    return(
-    <>
-        <header>
-            <a href="index.html">
-                <img src={logo} className="Logo1" alt="logo da equipe"/>
-            </a>
-            <nav>
-                <ul className="headerTxt">
-                    <li>
-                        <a href="index.html" id="inicio1">INICIO</a>
-                    </li>
-                    <li>
-                        <a href="sobre.html">SOBRE</a>
-                    </li>
-                    <li>
-                        <a href="açoes.html">AÇÕES</a>
-                    </li>
-                    <li>
-                        <a href="parceiros.html">PARCEIROS</a>
-                    </li>
-                    <li>
-                        <a href="competições.html">COMPETIÇÕES</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    </>
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+
+        console.log("funcionando")
+    }
+
+    return (
+        <>
+            <header>
+                <a href="index.html">
+                    <img src={logo} className="Logo1" alt="logo da equipe" />
+                </a>
+                <nav>
+                    <ul className="headerTxt">
+                        <div className="container">
+                        <li>
+                            <a href="index.html" id="inicio1">INICIO</a>
+                        </li>
+                        <li>
+                            <a href="sobre.html">SOBRE</a>
+                        </li>
+                        <li>
+                            <a href="açoes.html">AÇÕES</a>
+                        </li>
+                        <li>
+                            <a href="parceiros.html">PARCEIROS</a>
+                        </li>
+                        <li>
+                            <a href="competições.html">COMPETIÇÕES</a>
+                        </li>
+                        </div>
+
+                        <button className="menu" onClick={toggleMenu}>☰</button>
+                    </ul>
+
+
+                </nav>
+            </header>
+        </>
     )
 }
 
